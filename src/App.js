9167191d1cd55a2e954d9b76d0d5b8cc91a1a2f8
anksuunamun <shishkina_veronika_10@mail.ru>
2022-05-14 23:16:10 +0300

    feat: add 404 page, back btn (/characters/[id]), scroll behaviour

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from "react";
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Main from "./pages/Main";
 import Biography from "./pages/Biography";
 import Layout from "./components/Layout";
 import About from "./pages/About";
 import Contacts from "./pages/Contacts";
 import Characters from "./pages/Characters";
+import NotFound from "./pages/NotFound";
 
 function App() {
+  const {pathname, hash} = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      document.getElementById(hash.slice(1)).scrollIntoView({
+        block: 'center',
+        behavior: 'smooth'
+      });
+    } else {
+      window.scroll({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  }, [hash, pathname]);
+
   return (
     <Routes>
       <Route path="/" element={<Layout/>}>
         <Route index element={<Main/>}/>
-        <Route path="bio" element={<Characters />}/>
-        <Route path="bio/:id" element={<Biography />}/>
-        <Route path="about" element={<About />}/>
-        <Route path="contacts" element={<Contacts />}/>
+        <Route path="characters" element={<Characters/>}/>
+        <Route path="characters/:id" element={<Biography/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="contacts" element={<Contacts/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Route>
     </Routes>
   );
--- src/pages/Biography/Biography.js
+++ src/pages/Biography/Biography.js
@@ -1,18 +1,25 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams, Navigate } from "react-router-dom";
+import { BIO } from "../../constants/bio";
 import style from './Biography.module.scss';
 import Heading from "../../components/Heading";
 import Text from "../../components/Text";
 import Container from "../../components/Container";
-import { BIO } from "../../constants/bio";
+import Button from "../../components/Button";
+import { ReactComponent as Anchor } from './assets/link_icon.svg';
 
 
-const getBiographyBlock = (field, index) => {
+const getBiographyBlock = (field, index, handleAnchorClick) => {
   switch (field.type) {
     case 'h1': {
       return <Heading level={1} key={index}>{field.text}</Heading>
     }
     case 'h2': {
-      return <Heading level={2} key={index}>{field.text}</Heading>
+      return <Heading level={2} key={index} className={style.anchorHeading}>
+        {field.text}
+        <span className={style.anchor} id={getAnchorId(field.text)} onClick={handleAnchorClick}>
+          <Anchor/>
+        </span>
+      </Heading>
     }
     case 'paragraph': {
       return <Text element="p" className={style.paragraph} key={index}>{field.text}</Text>
@@ -28,13 +35,34 @@ const getBiographyBlock = (field, index) => {
   }
 };
 
+const getAnchorId = (text) => text.split(' ').join('_');
+
 const Biography = () => {
-  const { id } = useParams();
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const handleBackClick = () => navigate(-1);
+
+  const getHandleAnchorClick = (field) => () => {
+    const anchor = getAnchorId(field.text);
+    navigate(`#${anchor}`);
+  };
+
+  if (typeof BIO[id] === "undefined") {
+    return <Navigate to="/characters" replace/>
+  }
+
   return (
     <div className={style.root}>
       <div className={style.biography}>
+        <Button
+          theme="dark"
+          onClick={handleBackClick}
+        >
+          Back
+        </Button>
         <Container className={style.biographyContent}>
-          {BIO[id].map((field, index) => getBiographyBlock(field, index))}
+          {BIO[id].map((field, index) => getBiographyBlock(field, index, getHandleAnchorClick(field)))}
         </Container>
       </div>
     </div>
